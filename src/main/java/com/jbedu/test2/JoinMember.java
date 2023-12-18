4dd8d0f954e13c0c8845167ba4f61374f52061ce
gyojincompany <luckyred@gmail.com>
2023-12-18 15:37:34 +0900

    Form 입력 값 받기

---
+++ src/main/java/com/jbedu/test2/JoinMember.java
@@ -0,0 +1,48 @@
+package com.jbedu.test2;
+
+public class JoinMember {
+	
+	private String joinId;
+	private String joinPw;
+	private String joinName;
+	private String joinEmail;
+	
+	public JoinMember() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public JoinMember(String joinId, String joinPw, String joinName, String joinEmail) {
+		super();
+		this.joinId = joinId;
+		this.joinPw = joinPw;
+		this.joinName = joinName;
+		this.joinEmail = joinEmail;
+	}
+	public String getJoinId() {
+		return joinId;
+	}
+	public void setJoinId(String joinId) {
+		this.joinId = joinId;
+	}
+	public String getJoinPw() {
+		return joinPw;
+	}
+	public void setJoinPw(String joinPw) {
+		this.joinPw = joinPw;
+	}
+	public String getJoinName() {
+		return joinName;
+	}
+	public void setJoinName(String joinName) {
+		this.joinName = joinName;
+	}
+	public String getJoinEmail() {
+		return joinEmail;
+	}
+	public void setJoinEmail(String joinEmail) {
+		this.joinEmail = joinEmail;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/jbedu/test2/MemberController.java
@@ -0,0 +1,64 @@
+package com.jbedu.test2;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/loginOk")
+	public String loginOk(HttpServletRequest request, Model model) {
+		
+		String mid = request.getParameter("memberid");
+		String mpw = request.getParameter("memberpw");
+		
+		model.addAttribute("memberId", mid);
+		model.addAttribute("memberPw", mpw);		
+		
+		return "loginSuccess";
+	}
+	
+	@RequestMapping(value = "/loginOk2")
+	public String loginOk2(@RequestParam("memberid") String mid, @RequestParam("memberpw") String mpw, Model model) {
+		
+		model.addAttribute("memberId", mid);
+		model.addAttribute("memberPw", mpw);
+		
+		return "loginSuccess";		
+	}
+	
+	@RequestMapping(value = "/join")
+	public String join() {
+		return "join";
+	}
+	
+	@RequestMapping(value = "/joinOk")
+	public String joinOk(JoinMember joinMember) {
+		
+		return "joinOk";
+	}
+	
+	@RequestMapping(value = "/memberNum/{memberNumber}")
+	public String memberNum(@PathVariable String memberNumber, Model model) {
+		
+		model.addAttribute("memberNumber", memberNumber);
+		
+		return "memberNumOk";
+	}
+	
+	
+	
+
+}
